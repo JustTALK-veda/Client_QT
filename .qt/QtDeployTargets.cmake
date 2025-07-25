4e04569714d1e916c807dc2e43347a7821772ac2
kky107102 <kky107102@ajou.ac.kr>
2025-07-25 16:53:45 +0900

    chore: remove extra directories

--- .qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_JustTalk_FILE /Users/yj/Desktop/mytest/Client_QT/JustTalk.app/Contents/MacOS/JustTalk)
-set(__QT_DEPLOY_TARGET_JustTalk_TYPE EXECUTABLE)
--- CMakeFiles/JustTalk_autogen_timestamp_deps.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for JustTalk_autogen_timestamp_deps.
-# This may be replaced when dependencies are built.
