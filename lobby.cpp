3a0a3866ca3fa5b93f40d531dcd267e1a6694c8c
kky107102 <kky107102@ajou.ac.kr>
2025-07-28 10:53:55 +0900

    fead: add lobby design

--- lobby.cpp
+++ lobby.cpp
@@ -7,6 +7,20 @@ Lobby::Lobby(QWidget *parent)
     , ui(new Ui::Lobby)
 {
     ui->setupUi(this);
+    QPixmap vediooffPixmap(":/Image/config/video_off.png");
+    QPixmap vedioonPixmap(":/Image/config/video_on.png");
+    QPixmap micoffPixmap(":/Image/config/mic_off.png");
+    QPixmap miconPixmap(":/Image/config/mic_on.png");
+    ui->cam_icon_label->setPixmap(vediooffPixmap.scaled(
+        ui->cam_icon_label->size()
+        ));
+    ui->mic_icon_label->setPixmap(micoffPixmap.scaled(
+        ui->mic_icon_label->size(),
+        Qt::KeepAspectRatio,
+        Qt::SmoothTransformation
+        ));
+    connect(ui->cam_checkBox, &QCheckBox::toggled, ui->cameraWidget, &CameraWidget::setCamEnabled);
+    connect(ui->cam_checkBox, &QCheckBox::toggled, ui->cameraWidget, &CameraWidget::setMicEnabled);
     connect(ui->enterButton, &QPushButton::clicked, this, &Lobby::enterMeetingRequested);
 }
 
