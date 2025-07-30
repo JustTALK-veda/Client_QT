f417844d9c93f8740d7182b37497039a42543d27
kky107102 <kky107102@ajou.ac.kr>
2025-07-30 17:13:02 +0900

    feat: add ui

--- start.cpp
+++ start.cpp
@@ -3,22 +3,25 @@
 #include <QToolButton>
 #include <QIcon>
 #include <QSizePolicy>
+#include <QDateTime>
 
 Start::Start(QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Start)
 {
     ui->setupUi(this);
+    setStyleSheet(
+        "QLabel { color: white; }"
+        "QFrame#headerFrame { "
+        "background-color: #1E2939; "
+        "border-bottom: 1px solid #304159; "
+        "}"
+        );
 
-    // 전체 배경 설정
-    this->setStyleSheet("background-color: #101828;");
-
-
-    // 레이아웃 마진 및 간격 조정
-    ui->horizontalLayout->setContentsMargins(0, 0, 0, 0);
-    ui->horizontalLayout->setSpacing(0);
-    ui->horizontalLayout->setStretch(0, 1);
-    ui->horizontalLayout->setStretch(1, 1);
+    timeTimer = new QTimer(this);
+    connect(timeTimer, &QTimer::timeout, this, &Start::updateTime);
+    timeTimer->start(1000);
+    updateTime();
 
     // ───────── 회의실 버튼 ─────────
     ui->localUserButton->setIcon(QIcon(":/Image/config/local_user.png"));
@@ -72,94 +75,8 @@ Start::~Start()
     delete ui;
 }
 
-// #include "start.h"
-// #include <QLabel>
-// #include <QVBoxLayout>
-// #include <QHBoxLayout>
-// #include <QMouseEvent>
-// #include <QCursor>
-
-// // CustomButtonWidget 정의
-// class CustomButtonWidget : public QWidget {
-//     Q_OBJECT
-// public:
-//     CustomButtonWidget(const QString& iconPath,
-//                        const QString& titleText,
-//                        const QString& subtitleText,
-//                        QWidget* parent = nullptr)
-//         : QWidget(parent)
-//     {
-//         QVBoxLayout* layout = new QVBoxLayout(this);
-//         layout->setContentsMargins(0, 0, 0, 0);
-//         layout->setSpacing(10);
-//         setCursor(Qt::PointingHandCursor);
-
-//         QLabel* iconLabel = new QLabel;
-//         iconLabel->setPixmap(QPixmap(iconPath).scaled(150, 150, Qt::KeepAspectRatio, Qt::SmoothTransformation));
-//         iconLabel->setAlignment(Qt::AlignCenter);
-
-//         QLabel* textLabel = new QLabel;
-//         textLabel->setText(QString(R"(
-//             <div align='center'>
-//                 <span style='font-size:20pt; font-weight:bold; color:white;'>%1</span><br>
-//                 <span style='font-size:11pt; color:white;'>%2</span>
-//             </div>
-//         )").arg(titleText, subtitleText));
-//         textLabel->setAlignment(Qt::AlignCenter);
-//         textLabel->setStyleSheet("background-color: transparent;");
-
-//         layout->addWidget(iconLabel);
-//         layout->addWidget(textLabel);
-//         layout->setAlignment(Qt::AlignCenter);
-
-//         setStyleSheet("background-color: #101828;");
-//     }
-
-// signals:
-//     void clicked();
-
-// protected:
-//     void mousePressEvent(QMouseEvent* event) override {
-//         emit clicked();
-//         QWidget::mousePressEvent(event);
-//     }
-// };
-
-// // Start 클래스 생성자 구현
-// Start::Start(QWidget *parent)
-//     : QWidget(parent)
-// {
-//     QHBoxLayout* layout = new QHBoxLayout(this);
-//     layout->setContentsMargins(0, 0, 0, 0);
-//     layout->setSpacing(20);
-//     layout->setStretch(0, 1);
-//     layout->setStretch(1, 1);
-
-//     // 전체 배경
-//     this->setStyleSheet("background-color: #101828;");
-
-//     // 회의실 버튼
-//     auto* localButton = new CustomButtonWidget(
-//         ":/Image/config/local_user.png",
-//         "회의실",
-//         "새로운 회의를 시작하세요",
-//         this);
-//     connect(localButton, &CustomButtonWidget::clicked, this, &Start::enterLocalPageRequested);
-
-//     // 원격 참여자 버튼
-//     auto* remoteButton = new CustomButtonWidget(
-//         ":/Image/config/remote_user.png",
-//         "원격 참여자",
-//         "기존 회의에 참가하세요",
-//         this);
-//     connect(remoteButton, &CustomButtonWidget::clicked, this, &Start::enterRemotePageRequested);
-
-//     // 레이아웃에 추가
-//     layout->addWidget(localButton);
-//     layout->addWidget(remoteButton);
-// }
-
-// Start::~Start()
-// {
-//     // nothing to delete
-// }
+void Start::updateTime()
+{
+    QString currentTime = QDateTime::currentDateTime().toString("  hh:mm  ");
+    ui->timeLabel->setText(currentTime);
+}
